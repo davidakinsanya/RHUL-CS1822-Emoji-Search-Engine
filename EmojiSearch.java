cadc9d3d8644c40119fe195825c315cb04b2f315
davidakinsanya <58745820+davidakinsanya@users.noreply.github.com>
2019-12-13 20:57:54 +0000

    Update EmojiSearch.java

--- EmojiSearch.java
+++ EmojiSearch.java
@@ -13,7 +13,7 @@ class EmojiSearch {
             System.out.print("Enter what type of emoji you would like to search: ");
             String search_term = sc.nextLine();
             
-            while ((line = buffer.readLine())!= null) {
+            while ((line = buffer.readLine()) != null) {
             
                 for (int i = 0; i < line.length(); i++) { 
                     if (line.indexOf(search_term) != -1) {
